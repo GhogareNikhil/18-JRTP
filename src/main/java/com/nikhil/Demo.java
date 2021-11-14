7cf7cc95917950d4dc8634521cb904ccbd30f8c0
GhogareNikhil <ghogarenikhil00@gmail.com>
2021-11-14 19:42:02 +0530

    commit related to HIS-301

--- src/main/java/com/nikhil/Demo.java
+++ src/main/java/com/nikhil/Demo.java
@@ -2,9 +2,13 @@ package com.nikhil;
 
 public class Demo {
 
+  private map<String, object> cache = new HashMap<String, object>();
 	public void start()
 	{
 		// changes for HIS-122
 	}
-
+	//HIS-301 changes
+    public void loadDataToCache(){
+		//logic
+	}
 }
